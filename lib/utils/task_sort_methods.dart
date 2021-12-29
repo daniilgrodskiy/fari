98065db48ba7a533cb0f6bb4669dd24a2e9cf43b
Daniil Grodskiy <daniil.grodskiy@gmail.com>
2021-12-29 14:27:37 -0500

    Reverse dueDate method in task_sort_methods.dart

--- lib/utils/task_sort_methods.dart
+++ lib/utils/task_sort_methods.dart
@@ -39,29 +39,64 @@ class TaskSortMethods {
         // Both share the same day, so check to make sure both have a time
         if (task1.time != null && task2.time != null) {
           // Both have time, so sort by it and return opposite
-          return task1.time.compareTo(task2.time) == 1 ? -1 : 1;
+          return task1.time.compareTo(task2.time);
         } else {
           // One task does NOT have a time; put the general tasks on top
           if (task1.time == null) {
-            return -1;
-          } else {
-            // Since task1 does not have a time, we want it to be returned SECOND
             return 1;
+          } else {
+            // Since task1 DOES have a time, we want it to be returned SECOND
+            return -1;
           }
         }
       }
       // You can just sort by day because they have different days
-      return task1.day.compareTo(task2.day) == 1 ? -1 : 1;
+      return task1.day.compareTo(task2.day);
     } else {
       // At least one task does not have a date
       if (task1.day == null) {
         if (task2.day == null) {
           // Both tasks don't have a day, so sort by their creation date (id)
-          return task1.id.compareTo(task2.id) == 1 ? -1 : 1;
+          return task1.id.compareTo(task2.id);
         }
-        return -1;
+        return 1;
       }
-      return 1;
+      return -1;
     }
   }
+
+  // static int dueDateReverse(Task task1, Task task2) {
+  //   if (task1.day != null && task2.day != null) {
+  //     // Both tasks have a date
+  //     if (task1.day.year == task2.day.year &&
+  //         task1.day.month == task2.day.month &&
+  //         task1.day.day == task2.day.day) {
+  //       // Both share the same day, so check to make sure both have a time
+  //       if (task1.time != null && task2.time != null) {
+  //         // Both have time, so sort by it and return opposite
+  //         return task1.time.compareTo(task2.time) == 1 ? -1 : 1;
+  //       } else {
+  //         // One task does NOT have a time; put the general tasks on top
+  //         if (task1.time == null) {
+  //           return -1;
+  //         } else {
+  //           // Since task1 does not have a time, we want it to be returned SECOND
+  //           return 1;
+  //         }
+  //       }
+  //     }
+  //     // You can just sort by day because they have different days
+  //     return task1.day.compareTo(task2.day) == 1 ? -1 : 1;
+  //   } else {
+  //     // At least one task does not have a date
+  //     if (task1.day == null) {
+  //       if (task2.day == null) {
+  //         // Both tasks don't have a day, so sort by their creation date (id)
+  //         return task1.id.compareTo(task2.id) == 1 ? -1 : 1;
+  //       }
+  //       return -1;
+  //     }
+  //     return 1;
+  //   }
+  // }
 }
